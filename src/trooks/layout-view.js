8a804b3a1cece36aa4494c254e49a84dcba367d8
Lukas Fryc <lukas@fryc.eu>
2015-05-21 11:14:59 +0200

    included angular app inside marionette example

--- src/trooks/layout-view.js
+++ src/trooks/layout-view.js
@@ -1,11 +1,30 @@
 import {LayoutView} from 'backbone.marionette';
 import template from './layout-template.hbs';
+import angular from 'angular';
 
-export default LayoutView.extend({
-  template: template,
-  className: 'container',
-  regions: {
-    library : '.books__library',
-    viewer  : '.books__viewer'
-  }
-});
+export default (function() {
+  var root;
+
+  return LayoutView.extend({
+    template: template,
+    className: 'container',
+
+    onAttach: function () {
+      root = document.getElementById('angular-app');
+      angular.module('myApp', [])
+        .controller('MyCtrl', function ($scope) {
+          $scope.xyz = 123;
+        })
+        .config(function ($interpolateProvider) {
+          $interpolateProvider.startSymbol('#{').endSymbol('}');
+        });
+
+      angular.bootstrap(root, ['myApp']);
+    },
+
+    onDestroy: function () {
+      root.parentNode.remove(root);
+      root = null;
+    }
+  });
+})();
